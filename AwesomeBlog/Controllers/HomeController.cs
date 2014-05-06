b0e464f4f966223edf6b3d1b05e6ac717a9dcd3d
SeedPaths-User <lessdismissive@email.com>
2014-05-06 16:59:28 -0600

    AwesomeBlog

---
+++ AwesomeBlog/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AwesomeBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //step 1. set up database access
+        Models.myBlogEntities db = new Models.myBlogEntities();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var list = db.Posts.OrderByDescending(x => x.PostID);
+            return View(list);
+
+         
+        }
+
+    }
+}
---
+++ AwesomeBlog/Models/blogValidation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AwesomeBlog.Models
+{
+    [MetadataType(typeof(PostMetadata))]
+    public partial class Post
+    {
+        
+    }
+    public class PostMetadata
+    {
+        [Required(ErrorMessage="Title required"), Display(Name="Title")]
+        public string title;
+
+        [Required(ErrorMessage="Date required"), Display(Name="Date")]
+        public DateTime date;
+
+        [Required(ErrorMessage="Your name required"), Display(Name="Author")]
+        public string author;
+
+        [Display(Name="Likes")]
+        public int likes;
+
+        [Display(Name="Dislikes")]
+        public int dislikes;
+
+        [Display(Name="Views")]
+        public int views;
+
+        [Display(Name="Picture")]
+        public string picture;
+
+        [Required(ErrorMessage="Text required"), Display(Name="Body")]
+        public string body;
+
+
+    }
+
+
+}
